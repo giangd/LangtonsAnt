41a4b125a12038c0e547601338609fda051e507e
giangd <giangd@rocketmail.com>
2015-09-04 19:40:48 -0700

    first commit

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,17 +1,33 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 int total;
 
 Die bob = new Die();
 
-void setup() {
-  size(1500, 500);
+public void setup() {
+  size(500, 500);
   noStroke();
   textAlign(CENTER,CENTER);
   ellipseMode(CORNERS); //helps out with copying from gimp
   noLoop();
 }
 
-void draw() {
-  background(136, 225, 247);
+public void draw() {
+  background(random(255), random(255), random(255));
   
   for (int x=bob.wh/2; x<width; x+=50) {
     for (int y=bob.wh/2; y<height-bob.wh*2; y+=50) {
@@ -21,12 +37,12 @@ void draw() {
     }
   }
   
-  textSize(35);
-  text("Total: " + total, width/2, 464);
+  textSize(50);
+  text("Total: " + total, 250, 464);
   //total = 0;
 }
 
-void mousePressed() {
+public void mousePressed() {
   redraw();
   total = 0;
 }
@@ -46,7 +62,7 @@ class Die {
     value = (int)(Math.random()*6)+1;
   }
 
-  void show() {
+  public void show() {
     fill(random(255), random(255), random(255));
     rect(x, y, wh, wh, r);
 
@@ -79,6 +95,15 @@ class Die {
       ellipse(x+5, y+15, x+10, y+20);
       ellipse(x+5, y+10, x+10, y+15);
       ellipse(x+15, y+10, x+20, y+15);
+    }
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
     }
   }
 }
